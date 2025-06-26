37720b895d6350ffbd1fb901fccbeeb3ab145c43
Sılanur ÇANDIR <silanurcndr52@gmail.com>
2025-06-27 01:30:07 +0300

    Update pythonGiris.py

--- pythonGiris.py
+++ pythonGiris.py
@@ -10,9 +10,12 @@ print(y)
 x="python programming" #bir değişkenin türünü sonradan değiştirebiliyoruz
 print(x)
 
+#pythonda bu şekilde de yazılabilir
+x="python programming" ; print(x)
 #veriable casting(veri tipini özellikle belirtmek için yapılacak işlem)
 z=float(7)
 print(z)
+
 
 #veriable get the type with function
 print(type(z))
