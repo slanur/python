85c297abe0f9a454031eae44b1bbdec210dafd21
Sılanur ÇANDIR <silanurcndr52@gmail.com>
2025-07-02 20:38:00 +0300

    Update my_module.py

--- my_module.py
+++ my_module.py
@@ -1,7 +1,9 @@
 number=7 
+#yazdığımız kodu .py uzantılı bir dosyaya kaydediyoruz. örneğin my_modüle olarak yazmış olalım
 #kendi modülümüzü kullanmak istediğimiz dosyada import my_modüle ile kullanabiliriz 
 #modüle takma ad oluşturmak:
-#import my_modüle as m--> bu şekilde modülümüze m takma adını verdik  
+#import my_modüle as m --> bu şekilde modülümüze m takma adını verdik  
+#print(dir(m)) ile m modülü içinde kullandığımız tüm fonksiyonları görebiliriz
 #pythondaki diğer modülleri internette  built-in yazarak bulabiliriz
 
 #bir moduledeki fonksiyonları kullanmak istediğimizde sürekli modülün_adı.işlem şeklinde uzun uzun kullnamak istemiyorsak from datetime import * şeklinde başına modül ismi yazmadan kullanabilriz
